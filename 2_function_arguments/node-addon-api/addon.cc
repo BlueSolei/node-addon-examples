75d9a4e0406a74fa54cafc2c1e7fa32ae172793d
Shaul Fridman <shaul.fridman@gmail.com>
2019-03-16 20:38:12 +0200

    Factor out the C++ code from the JS binding code

--- 2_function_arguments/node-addon-api/addon.cc
+++ 2_function_arguments/node-addon-api/addon.cc
@@ -1,5 +1,7 @@
 #include <napi.h>
 
+double AddNative(double a, double b) { return a + b; }
+
 Napi::Value Add(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
 
@@ -15,7 +17,7 @@ Napi::Value Add(const Napi::CallbackInfo& info) {
 
   double arg0 = info[0].As<Napi::Number>().DoubleValue();
   double arg1 = info[1].As<Napi::Number>().DoubleValue();
-  Napi::Number num = Napi::Number::New(env, arg0 + arg1);
+  Napi::Number num = Napi::Number::New(env, AddNative(arg0, arg1));
 
   return num;
 }
