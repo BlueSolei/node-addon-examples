2de185074e5e0af35e11c0ce0a6692141415d93e
Shaul Fridman <shaul.fridman@gmail.com>
2019-03-16 20:45:54 +0200

    (+) WIP: Bind a function with paramters.
    The sample code for this binding.

--- 1_hello_world/cpp-napi/hello.cc
+++ 1_hello_world/cpp-napi/hello.cc
@@ -7,9 +7,37 @@
 std::string World() { return "world"; }
 std::string Hello() { return "hello"; }
 
+double AddNative(double a, double b) { return a + b; }
+
+Napi::Value Add(const Napi::CallbackInfo &info)
+{
+  Napi::Env env = info.Env();
+
+  if (info.Length() < 2)
+  {
+    Napi::TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  if (!info[0].IsNumber() || !info[1].IsNumber())
+  {
+    Napi::TypeError::New(env, "Wrong arguments").ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  double arg0 = info[0].As<Napi::Number>().DoubleValue();
+  double arg1 = info[1].As<Napi::Number>().DoubleValue();
+  Napi::Number num = Napi::Number::New(env, AddNative(arg0, arg1));
+
+  return num;
+}
+
 Napi::Object Init(Napi::Env env, Napi::Object exports)
 {
-  return Module(env, exports).Register("hello", Hello).Register("world", World);
+  auto module = Module(env, exports).Register("hello", Hello).Register("world", World);
+  exports.Set(Napi::String::New(env, "add"),
+              Napi::Function::New(env, Add));
+  return module;
 }
 
 NODE_API_MODULE(hello, Init)
--- 1_hello_world/cpp-napi/hello.js
+++ 1_hello_world/cpp-napi/hello.js
@@ -1,4 +1,5 @@
-var addon = require('bindings')('hello');
+var addon = require("bindings")("hello");
 
 console.log(addon.hello()); // 'hello'
 console.log(addon.world()); // 'world'
+console.log(`2 + 3 = ${addon.add(2, 3)}`);
